597e3ea574ac6811381103a2ca71f1dde1f530b7
chrishennessy <58816114+chrishennessy@users.noreply.github.com>
2020-02-10 23:23:27 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Chris Hennessy on 2/10/20.
+//  Copyright © 2020 Chris Hennessy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
